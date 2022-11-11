fce6d1b71b5424c26b4cdee171226319662de422
Caio Marcatti <caio.marcatti@rb.com.br>
2022-11-11 18:01:57 -0300

    fix moment launcher

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,31 +11,31 @@ use CaioMarcatti12\Repository\TipoEnum;
 use CaioMarcatti12\Data\ObjectMapper;
 use Illuminate\Support\Str;
 use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase {
-    public function testObjectMapperToArray():void{
-        $destinatarioObjetoValor = new DestinatarioObjetoValor('caio', 'test');
-        $mensagemEntidade = new MensagemEntidade($destinatarioObjetoValor, TipoEnum::EMAIL);
-        $mensagemEntidade = new \stdClass();
-        $mensagemEntidade->teste = 1;
-        $mensagemEntidade->caio = new \stdClass();
-        $mensagemEntidade->caio->teste = 2;
-        $array = ObjectMapper::toArray($mensagemEntidade);
-
-        var_dump($array);
-    }
-
-    public function testConnectionMongoDB():void{
-        $mongoDB = new MongoDB();
-        $mongoDB->handler();
-
-        $modelo = new ModeloMensagem();
-        $modelo->mensagem = 'test';
-        $modelo->save();
-    }
-
-    public function testConnectionMysql():void{
-        $mysql = new Mysql();
-        $mysql->handler();
-    }
-}
+//
+//class ExampleTest extends TestCase {
+//    public function testObjectMapperToArray():void{
+//        $destinatarioObjetoValor = new DestinatarioObjetoValor('caio', 'test');
+//        $mensagemEntidade = new MensagemEntidade($destinatarioObjetoValor, TipoEnum::EMAIL);
+//        $mensagemEntidade = new \stdClass();
+//        $mensagemEntidade->teste = 1;
+//        $mensagemEntidade->caio = new \stdClass();
+//        $mensagemEntidade->caio->teste = 2;
+//        $array = ObjectMapper::toArray($mensagemEntidade);
+//
+//        var_dump($array);
+//    }
+//
+//    public function testConnectionMongoDB():void{
+//        $mongoDB = new MongoDB();
+//        $mongoDB->handler();
+//
+//        $modelo = new ModeloMensagem();
+//        $modelo->mensagem = 'test';
+//        $modelo->save();
+//    }
+//
+//    public function testConnectionMysql():void{
+//        $mysql = new Mysql();
+//        $mysql->handler();
+//    }
+//}
